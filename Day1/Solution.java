638bbfe51b2d8259b04f3a02935e94db8ea68bfb
VenkyGitRep <venks.mail@gmail.com>
2022-06-01 17:55:13 -0400

    Remove problem details from java file

--- Day1/Solution.java
+++ Day1/Solution.java
@@ -1,5 +1,5 @@
-//Problem : 1480. Running Sum of 1d Array
-//url : https://leetcode.com/problems/running-sum-of-1d-array/
+package Day1;
+
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] runningSum = new int[nums.length];
