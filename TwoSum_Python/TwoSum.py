8ab99a00cbe733103f09b4accdc3ba85a979b956
Venky Sundar MacBook Pro <venks.mail@gmail.com>
2024-04-03 19:15:29 -0400

    Changes in Python file so it can be run locally.

--- TwoSum_Python/TwoSum.py
+++ TwoSum_Python/TwoSum.py
@@ -1,11 +1,17 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myMap = {}
+from typing import List
 
-        for i in range(len(nums)):
-            if target - nums[i] in myMap:
-                return [myMap[target - nums[i]], i]
-            myMap[nums[i]] = i
+def twoSum(nums: List[int], target: int) -> List[int]:
+    myMap = {}
 
-        return []        
+    for i in range(len(nums)):
+        if target - nums[i] in myMap:
+            return [myMap[target - nums[i]], i]
+        myMap[nums[i]] = i
+
+    return []        
         
+if __name__=="__main__":
+    print('Yo, wassup')
+    print(twoSum([1,2,3,4,5,6,7,8],4))
+
+print('Yo, wassupg')
