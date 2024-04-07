02d06e7b81ff234e36741ec65331685cf8322968
Venky Sundar MacBook Pro <venks.mail@gmail.com>
2024-04-06 20:36:11 -0400

    Accepted solution for Triplet subsequence

---
+++ IncreatingTripletSubsequence/Solution.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first_num = float("inf")
+        second_num = float("inf")
+        for n in nums:
+            if n <= first_num:
+                first_num = n
+            elif n <= second_num:
+                second_num = n
+            else:
+                return True
+        return False
+        
+        
+if __name__=="__main__":
+    sol = Solution()
+    print(sol.increasingTriplet([20,100,10,12,5,13]))
+        
+        
